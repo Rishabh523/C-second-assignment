328947336f7e4ff4a4ef5a2263199b593e87812f
Rishabh Pratap Singh <116909258+Rishabh523@users.noreply.github.com>
2024-07-30 09:08:03 +0530

    Create Remove a file from the disk.cs

---
+++ Remove a file from the disk.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // Specify the file path
+        string filePathname = @"D:\\MyFile.txt";
+
+        try
+        {
+            // Check if the file exists
+            if (File.Exists(filePathname))
+            {
+                // Delete the file
+                File.Delete(filePathname);
+                Console.WriteLine("The file has been deleted successfully.");
+            }
+            else
+            {
+                Console.WriteLine("The file does not exist.");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+        Console.ReadKey();
+    }
+}
